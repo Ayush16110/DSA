a8df91fd6a54d64b0c64cc724c8cd2d8938f911e
Ayush Narayan Gupta <102436959+Ayush16110@users.noreply.github.com>
2026-09-03 18:18:50 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (47.61%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        if (intervals.size() == 0)
-            return {newInterval};
-
-        auto it =
-            lower_bound(intervals.begin(), intervals.end(), newInterval[0],
-                        [](const vector<int>& interval, int value) {
-                            return interval[0] < value;
-                        });
-
-        intervals.insert(it, newInterval);
-        vector<vector<int>> mergeInterval;
-        mergeInterval.push_back(intervals[0]);
-        for(auto curr : intervals) {
-            if(mergeInterval.back()[1] >= curr[0]) {
-                mergeInterval.back()[1] = max(mergeInterval.back()[1], curr[1]);
-            } else {
-                mergeInterval.push_back(curr);
-            }
+        if(intervals.size() == 0) return {newInterval};
+        int i = 0;
+        vector<vector<int>> ans;
+        while(i < intervals.size() && intervals[i][1] < newInterval[0]) {
+            ans.push_back(intervals[i]);
+            i++;
         }
 
-        return mergeInterval;
+        vector<int> OverlappedInterval = newInterval;
+
+        while(i < intervals.size() && intervals[i][0] <= newInterval[1] && newInterval[0] <= intervals[i][1]) {
+            OverlappedInterval[0] = min(intervals[i][0], OverlappedInterval[0]);
+            OverlappedInterval[1] = max(intervals[i][1], OverlappedInterval[1]);
+            i++;
+        }
+
+        ans.push_back(OverlappedInterval);
+        
+        while(i < intervals.size() && i < intervals.size()) {
+            ans.push_back(intervals[i]);
+            i++;
+        }
+
+        return ans;
     }
 };
