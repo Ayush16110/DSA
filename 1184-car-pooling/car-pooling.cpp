1bf2f0bc15801a0895eab263d47a860299e9895f
Ayush Narayan Gupta <102436959+Ayush16110@users.noreply.github.com>
2026-09-06 18:40:01 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (15.96%) - LeetSync

--- 1184-car-pooling/car-pooling.cpp
+++ 1184-car-pooling/car-pooling.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
-        vector<int> nums(1000, 0);
+        vector<int> temp(1001);
 
         for(auto t : trips) {
-            int l = t[1], r = t[2], x = t[0];
+            int l = t[1];
+            int r = t[2];
+            int x = t[0];
 
-            nums[l] += x;
-            if(r < 1000) nums[r] -= x;
+            temp[l] += x;
+            temp[r] -= x;
+        }
+        
+        int sum = 0;
+        for(int t : temp) {
+            sum += t;
+            if(sum > capacity) return false;
         }
 
-        for(int i = 1; i < 1000; i++) {
-            nums[i] += nums[i-1];
-        }
-
-        return capacity >= *max_element(nums.begin(), nums.end());
+        return true;
     }
 };
