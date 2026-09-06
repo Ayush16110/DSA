f0fa41a8b2ca75220f10da853e601bf94e320674
Ayush Narayan Gupta <102436959+Ayush16110@users.noreply.github.com>
2026-09-06 22:30:27 +0530

    Time: 14 ms (88.13%) | Memory: 89.4 MB (9.67%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -2,26 +2,24 @@ class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
         int n = heights.size();
-        stack<int> s;
         vector<int> left(n, 0), right(n, n-1);
+        stack<int> s;
 
-        for (int i = 0; i < n; i++) {
-            while (!s.empty() && heights[i] < heights[s.top()]) {
+        for(int i = 0; i < n; i++) {
+            while(!s.empty() && heights[s.top()] > heights[i]) {
                 right[s.top()] = i - 1;
                 s.pop();
             }
-
             s.push(i);
         }
 
         while(!s.empty()) s.pop();
 
-        for (int i = n-1; i >= 0; i--) {
-            while (!s.empty() && heights[i] < heights[s.top()]) {
+        for(int i = n-1; i >= 0; i--) {
+            while(!s.empty() && heights[s.top()] > heights[i]) {
                 left[s.top()] = i + 1;
                 s.pop();
             }
-
             s.push(i);
         }
 
@@ -29,8 +27,7 @@ public:
 
         for(int i = 0; i < n; i++) {
             int width = right[i] - left[i] + 1;
-            int area = heights[i] * width;
-
+            int area = width * heights[i];
             ans = max(ans, area);
         }
 
