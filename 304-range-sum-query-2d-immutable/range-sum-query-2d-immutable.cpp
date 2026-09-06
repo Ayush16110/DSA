48bffb31964b59633d4cf5a00a40ca9b0282aecf
Ayush Narayan Gupta <102436959+Ayush16110@users.noreply.github.com>
2026-09-06 19:17:42 +0530

    Time: 21 ms (69.79%) | Memory: 152.3 MB (5.56%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.cpp
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.cpp
@@ -1,25 +1,25 @@
 class NumMatrix {
-vector<vector<int>> matrix;
-vector<vector<int>> prefix;
+private:
+    vector<vector<int>> matrix, prefix;
 public:
     NumMatrix(vector<vector<int>>& matrix) {
         this->matrix = matrix;
         this->prefix = setPrefix(matrix);
     }
 
-    vector<vector<int>> setPrefix(const vector<vector<int>>& matrix) {
+    vector<vector<int>> setPrefix(vector<vector<int>>& matrix) {
         vector<vector<int>> prefix(matrix.size() + 1, vector<int> (matrix[0].size() + 1, 0));
         for(int i = 0; i < matrix.size(); i++) {
             for(int j = 0; j < matrix[0].size(); j++) {
-                prefix[i+1][j+1] = matrix[i][j] + prefix[i][j+1] + prefix[i+1][j] - prefix[i][j];
+                prefix[i+1][j+1] = matrix[i][j] + prefix[i+1][j] + prefix[i][j+1] - prefix[i][j];
             }
         }
-
+        
         return prefix;
     }
     
     int sumRegion(int row1, int col1, int row2, int col2) {
-        return (this->prefix[row2+1][col2+1] - this->prefix[row2+1][col1] - this->prefix[row1][col2+1] + this->prefix[row1][col1]);
+        return this->prefix[row2 + 1][col2 + 1] - this->prefix[row2 + 1][col1] - this->prefix[row1][col2 + 1] + this->prefix[row1][col1];
     }
 };
 
