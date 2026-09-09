9cc546faa13a4835a1f1b4b9993526cd2d1488e2
Ayush Narayan Gupta <102436959+Ayush16110@users.noreply.github.com>
2026-09-09 16:31:17 +0530

    Time: 24 ms (8.14%) | Memory: 67.7 MB (97.33%) - LeetSync

--- 456-132-pattern/132-pattern.cpp
+++ 456-132-pattern/132-pattern.cpp
@@ -1,41 +1,30 @@
 class Solution {
 public:
     bool find132pattern(vector<int>& nums) {
-        // vector<int> minLeft(n);
-        // minLeft[0] = nums[0];
-        // for(int i = 1; i < n; i++) {
-        //     minLeft[i] = min(minLeft[i-1], nums[i]);
-        // }
-
-        // for(int i = 1; i < n; i++) {
-        //     if(minLeft[i-1] >= nums[i]) continue;
-        //     for(int j = i + 1; j < n; j++) {
-        //         if(nums[j] >= nums[i]) continue;
-        //         if(nums[j] > minLeft[i-1]) return true;
-        //     }
-        // }
-
-        // return false;
-
         int n = nums.size();
-        if(n < 3) return false;
+        if (n < 3) return false;
+
         stack<pair<int, int>> st;
-        st.push({nums[0], nums[0]});
+
         int minLeft = nums[0];
 
-        for(int i = 1; i < n; i++) {
-            minLeft = min(minLeft, nums[i]);
-            while(!st.empty() && nums[i] >= st.top().first) {
+        for (int i = 1; i < n; i++) {
+            int prevMin = minLeft;
+
+            while (!st.empty() && nums[i] >= st.top().first) {
                 st.pop();
             }
-            if(!st.empty() && nums[i] < st.top().first && nums[i] > st.top().second) {
+
+            if (!st.empty() &&
+                nums[i] > st.top().second) {
                 return true;
             }
-            else {
-                st.push({nums[i], minLeft});
-            }
 
+            st.push({nums[i], prevMin});
+
+            minLeft = min(minLeft, nums[i]);
         }
+
         return false;
     }
 };
