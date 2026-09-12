9f6a0f8e8c6972e6f92b11f381a8bfff3db77f4d
Ayush Narayan Gupta <102436959+Ayush16110@users.noreply.github.com>
2026-09-12 20:00:29 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (49.72%) - LeetSync

---
+++ 316-remove-duplicate-letters/remove-duplicate-letters.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        unordered_map<char, int> lastOcc;
+        string st;
+        vector<bool> present(26, 0);
+
+        for(int i = 0; i < s.length(); i++) {
+            lastOcc[s[i]] = i;
+        }
+
+        for(int i = 0; i < s.length(); i++) {
+            if(present[s[i] - 'a'] == 1) continue;
+            while(!st.empty() and st.back() > s[i] and i < lastOcc[st.back()]) {
+                present[st.back() - 'a'] = 0;
+                st.pop_back();
+            }
+
+            st.push_back(s[i]);
+            present[st.back() - 'a'] = 1;
+        }
+
+        return st;
+    }
+};
