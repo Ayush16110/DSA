8d9125677bd816d990234fa56a891c514d18c012
Ayush Narayan Gupta <102436959+Ayush16110@users.noreply.github.com>
2026-09-12 21:45:50 +0530

    Time: 27 ms (97.84%) | Memory: 105.8 MB (60.12%) - LeetSync

--- 883-car-fleet/car-fleet.cpp
+++ 883-car-fleet/car-fleet.cpp
@@ -3,24 +3,25 @@ public:
     int carFleet(int target, vector<int>& position, vector<int>& speed) {
         vector<pair<int, int>> cars;
 
-        for (int i = 0; i < position.size(); i++) {
+        for(int i = 0; i < position.size(); i++) {
             cars.push_back({position[i], speed[i]});
         }
 
-        sort(cars.begin(), cars.end(),[](auto a, auto b) {
-            return a.first > b.first; 
+        sort(cars.begin(), cars.end(), [](auto a, auto b){
+            return a.first > b.first;
         });
 
+        stack<double> s;
         int count = 0;
-        stack<double> st;
 
-        for (auto& car : cars) {
-            double time = (double)(target - car.first) / car.second;
-            if (!st.empty() && time <= st.top()) {
+        for(int i = 0; i < cars.size(); i++) {
+            double time = (double)(target - cars[i].first) / cars[i].second;
+
+            if(!s.empty() and s.top() >= time) {
                 continue;
             }
-            st.push(time);
             count++;
+            s.push(time);
         }
 
         return count;
